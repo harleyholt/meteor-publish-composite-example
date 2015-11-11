a776372cd1c5f3c2dd931a63be89ed03e116889c
Harley Holt <harley.holt@csats.com>
2015-11-11 09:38:20 -0800

    Send more information to statsd
    Memory, heap size
    Mongo queries not working

--- app/server/appmetrics.js
+++ app/server/appmetrics.js
@@ -1,11 +1,35 @@
+
+/**
+ * This starts a process of collecting and reporting our process metrics to the statsd server.
+ */
 Meteor.startup(function(){
   const appmetrics = Meteor.npmRequire('appmetrics');
-  const monitor = appmetrics.monitor();
   const StatsD = Meteor.npmRequire('node-statsd');
-  const statsd = new StatsD({port: 48125, prefix: 'mpc.', cacheDns: true});
 
+  const statsd = new StatsD({port: 48125, prefix: 'mpc.', cacheDns: true});
+  const monitor = appmetrics.monitor();
 
   monitor.on('cpu', function(cpu) {
+    // Percentage of CPU used by the process.
+    // This is reported as percentage of the total CPU power of the system; the process may
+    // be using 100% of the core it is running on, but will only show some percent of the total
     statsd.gauge('server.cpu.process', cpu.process);
+  });
+
+  monitor.on('memory', function(memory) {
+    // Bytes of memory assigned to the process that are not shared with other processes.
+    statsd.gauge('server.memory.private', memory.private);
+  });
+
+  monitor.on('gc', function(gc) {
+    // Size of the process heap in bytes
+    statsd.gauge('gc.used', gc.used);
+  });
+
+  monitor.on('mongo', function(mongo) {
+    // A mongo query was issued
+    // TODO: This is not working. First thought is that meteor is not using the same mongo
+    // library that app metrics is hooking into. Investigate.
+    statsd.counter('mongo.query');
   });
 });
